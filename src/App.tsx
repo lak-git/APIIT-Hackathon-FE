77d247dd618c8dc67ce34e9b4c12e60fe9034c58
stefanshabbir <stefanshabbir@gmail.com>
2025-12-13 15:36:56 +0530

    feat: Login/Registration Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import { LoginScreen } from "./app/components/LoginScreen";
 
 import { IncidentProvider } from "./providers/IncidentProvider";
 
 
 export default function App() {
 	return (
-		<IncidentProvider>
-			<div className="min-h-screen bg-[#FAF3E8] text-[#4A1A1A]">
+		<LoginScreen onLogin={(email, password) => { }} />
+		// <IncidentProvider>
+		// 	<div className="min-h-screen bg-[#FAF3E8] text-[#4A1A1A]">
 
-				<main className="mx-auto w-full max-w-6xl px-4 py-6">
-					<Outlet />
-				</main>
-			</div>
-			<Toaster position="top-center" richColors />
-		</IncidentProvider>
+		// 		<main className="mx-auto w-full max-w-6xl px-4 py-6">
+		// 			<Outlet />
+		// 		</main>
+		// 	</div>
+		// 	<Toaster position="top-center" richColors />
+		// </IncidentProvider>
 	);
 }
